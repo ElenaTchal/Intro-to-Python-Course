c924e7037a2e79a19981e3f82a04f1811c6914b0
Elena Tchalakov <71348324+ElenaTchal@users.noreply.github.com>
2020-09-26 13:08:28 -0700

    Update Assignment 4.6.py

--- Assignment 4.6.py
+++ Assignment 4.6.py
@@ -14,18 +14,3 @@ else:
 
 p = computepay(h,r)
 print("Pay",p)
-
-
-
-
-# hrs = input("Enter Hours:")
-# h = float(hrs)
-# rate = input("Enter Rate:")
-# r = float(rate)
-# if h>40:
-    #reg = 40*r
-    #otp = (h-40)*((r*1.5))
-    #total = reg+otp
-    #print(total)
-#else:
-   # print(r*h)
