3165cb903568060c5b0cad989873374a4205698c
Elena Tchalakov <71348324+ElenaTchal@users.noreply.github.com>
2020-09-26 13:08:50 -0700

    Update Assignment 5.2.py

--- Assignment 5.2.py
+++ Assignment 5.2.py
@@ -22,18 +22,3 @@ while True:
         continue
 print("Maximum is", largest)
 print("Minimum is", smallest)
-
-
-
-
-# hrs = input("Enter Hours:")
-# h = float(hrs)
-# rate = input("Enter Rate:")
-# r = float(rate)
-# if h>40:
-    #reg = 40*r
-    #otp = (h-40)*((r*1.5))
-    #total = reg+otp
-    #print(total)
-#else:
-   # print(r*h)
